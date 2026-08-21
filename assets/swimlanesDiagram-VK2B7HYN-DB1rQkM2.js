276cf9b0500153baff55ce40817a37761290c285
Matheus Emanoel <matheusemanoel@ucl.br>
2026-08-21 18:01:47 -0300

    deploy: corrige nome do wasm do sql.js (sql-wasm-browser.wasm)

---
+++ assets/swimlanesDiagram-VK2B7HYN-DB1rQkM2.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-A5DVABFB-CF-GS4g9.js";import{_ as a}from"./index-CqAxruE7.js";import"./chunk-5VM5RSS4-SYcIHlW1.js";import"./chunk-XXDRQBXY-CV3SL_rm.js";import"./chunk-POPQ4Y6H-AbOamA7s.js";import"./chunk-F27PBJKO-D4li1B9R.js";import"./channel-CKrvR6gU.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
