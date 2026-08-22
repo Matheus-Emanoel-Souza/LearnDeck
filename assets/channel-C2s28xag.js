6c84f8521280b12f8ea0ba3f00757465b23717e7
Matheus-Emanoel-Souza <matheusemanoel@ucl.br>
2026-08-22 10:48:17 -0300

    deploy: build web com layout responsivo e sidebar minimizavel
    Regenerado a partir da branch webapp com o main mesclado (PR #1),
    substituindo a landing page estatica pelo app funcional.

--- assets/channel-C2s28xag.js
+++
@@ -1,7 +0,0 @@
-import { al as Utils, am as Color } from "./index-BFtmwBVq.js";
-const channel = (color, channel2) => {
-  return Utils.lang.round(Color.parse(color)[channel2]);
-};
-export {
-  channel as c
-};
