1bb77b521dba48c5fc484c35da9fbf82bb9c769c
Matheus-Emanoel-Souza <matheusemanoel@ucl.br>
2026-08-22 20:48:25 -0300

    deploy: layout tatil por telas no mobile
    Barra de abas no rodape, quadro em drill-down e Kanban em carrossel abaixo
    de 720px. Inclui a correcao do service worker (rede-primeiro no HTML).

---
+++ assets/swimlanesDiagram-VK2B7HYN-pc124pqP.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-A5DVABFB-B5Mvxi3m.js";import{_ as a}from"./index-D3uP2eiR.js";import"./chunk-5VM5RSS4-CbNmPp8s.js";import"./chunk-XXDRQBXY-C3dOqdEF.js";import"./chunk-POPQ4Y6H-DD5Y0_vH.js";import"./chunk-F27PBJKO-TvAk9qve.js";import"./channel-DANJuMGu.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
