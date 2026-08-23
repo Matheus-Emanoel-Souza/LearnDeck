83cef9b84063129f5b8d58b04f7c2d1881c14762
Matheus Emanoel <152511027+Matheus-Emanoel-Souza@users.noreply.github.com>
2026-08-23 02:28:46 -0300

    deploy: logo oficial - baralho de cartas azul/dourado

--- assets/swimlanesDiagram-VK2B7HYN-Bzxgp-Jq.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-A5DVABFB-CPB-oMr1.js";import{_ as a}from"./index-CCnDl2S-.js";import"./chunk-5VM5RSS4-D2_Jd3Nm.js";import"./chunk-XXDRQBXY-qCUc42fr.js";import"./chunk-POPQ4Y6H-D3ieizDW.js";import"./chunk-F27PBJKO-C9OjeNJb.js";import"./channel-Cz7eWNvE.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
