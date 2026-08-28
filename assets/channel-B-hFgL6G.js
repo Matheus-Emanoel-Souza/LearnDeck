9f875305dd616cff271bbdbce5c022e41e65e677
Matheus Emanoel <152511027+Matheus-Emanoel-Souza@users.noreply.github.com>
2026-08-28 00:09:26 -0300

    deploy: toque e segure funciona na coluna inteira, nao so no cabecalho

---
+++ assets/channel-B-hFgL6G.js
@@ -0,0 +1,1 @@
+import{al as o,am as n}from"./index-3JDu4LbD.js";const l=(a,r)=>o.lang.round(n.parse(a)[r]);export{l as c};
---
+++ assets/swimlanesDiagram-VK2B7HYN-DMrcTJwG.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-A5DVABFB-BPYfiCX5.js";import{_ as a}from"./index-3JDu4LbD.js";import"./chunk-5VM5RSS4-DNjBFD3e.js";import"./chunk-XXDRQBXY-C9rSNglE.js";import"./chunk-POPQ4Y6H-CNbAYkAJ.js";import"./chunk-F27PBJKO-Cr1MjBWH.js";import"./channel-B-hFgL6G.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
