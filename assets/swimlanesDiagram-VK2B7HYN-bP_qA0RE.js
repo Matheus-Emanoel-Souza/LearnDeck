820b17397a682f89b3545c5e6b7d8730572d38cd
Matheus Emanoel <152511027+Matheus-Emanoel-Souza@users.noreply.github.com>
2026-08-27 23:56:56 -0300

    deploy: toque e segure nas colunas + checagem de atualizacao real (PWA)

--- assets/swimlanesDiagram-VK2B7HYN-bP_qA0RE.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-A5DVABFB-Bi8EnozP.js";import{_ as a}from"./index-MItbRbT3.js";import"./chunk-5VM5RSS4-D6uill4g.js";import"./chunk-XXDRQBXY-EH_Qg_Qh.js";import"./chunk-POPQ4Y6H-Cpb-Rlvc.js";import"./chunk-F27PBJKO-COGlXKa7.js";import"./channel-BuwK8-Eq.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
